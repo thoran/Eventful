47862a66cd64966f00d641176da718fd40e29c0c
thoran <code@thoran.com>
2014-07-28 15:29:14 +1000

    Committed automatically by git-importall.

--- lib/Eventful.rb
+++ lib/Eventful.rb
@@ -1,30 +1,28 @@
 # Eventful.rb
 # Eventful
 
-# 20140112, 0202
-# 0.0.0
+# 20140202
+# 0.0.1
 
 require 'Stateful'
 
 module Eventful
 
   # memory resident
-  def run
+  def run_loop
     loop do
-      self.active.all.each do |instance|
-        instance.state.transitions.each do |transition|
-          if instance.send("#{transition.event_name}?")
-            instance.send("#{transition.event_name}")
-          end
-        end
-      end
+      run
     end
   end
 
   # scheduled
   def run
     self.active.all.each do |instance|
-
+      instance.state.transitions.each do |transition|
+        if instance.send("#{transition.event_name}?")
+          instance.send("#{transition.event_name}")
+        end
+      end
     end
   end
 
