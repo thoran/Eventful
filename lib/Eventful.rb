0040770572136f13b554a5c16bd84b4337e0e993
thoran <code@thoran.com>
2014-07-28 15:29:14 +1000

    Committed automatically by git-importall.

--- lib/Eventful.rb
+++ lib/Eventful.rb
@@ -2,14 +2,14 @@
 # Eventful
 
 # 20140203
-# 0.1.0
+# 0.2.0
 
 require 'Stateful'
 
 module Eventful
 
   # memory resident
-  def run_loop(frequency_in_seconds = 0)
+  def memory_run(frequency_in_seconds = 0)
     loop do
       sleep frequency_in_seconds
       run
@@ -17,13 +17,21 @@ module Eventful
   end
 
   # scheduled
-  def run
+  def scheduled_run
     self.active.all.each do |instance|
       instance.state.transitions.each do |transition|
         if instance.send("#{transition.event_name}?")
           instance.send("#{transition.event_name}")
         end
       end
+    end
+  end
+
+  def run(frequency_in_seconds = nil)
+    if frequency_in_seconds
+      memory_run(frequency_in_seconds)
+    else
+      scheduled_run
     end
   end
 
