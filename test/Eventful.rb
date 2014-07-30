e38441cb70cf80b7c0b7b00cf6114c144e6c3c77
thoran <code@thoran.com>
2014-07-30 22:15:20 +1000

    - Kernel/with_warning as it wasn't being used in this test suite.

--- test/Eventful.rb
+++ test/Eventful.rb
@@ -9,8 +9,6 @@ require 'minitest-spec-context'
 test_dir = File.dirname(File.expand_path(__FILE__))
 $LOAD_PATH.unshift(test_dir) unless $LOAD_PATH.include?(test_dir)
 
-require 'Kernel/with_warning'
-
 lib_dir = File.expand_path(File.join(test_dir, '..', 'lib'))
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 
